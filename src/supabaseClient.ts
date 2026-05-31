c9391ddd93f7039f320bbc7458344844f861f01a
taphysics <sanjeewadanushka455@gmail.com>
2026-05-31 17:07:39 +0530

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,13 +1,7 @@
-// @ts-ignore
 import { createClient } from '@supabase/supabase-js'
 
-// @ts-ignore
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-// @ts-ignore
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// කෙළින්ම ඔයාගේ ඇත්තම Supabase දත්ත මෙතනට ලබා දී ඇත
+const supabaseUrl = 'https://yopepkpwmnzygynjwszh.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvcGVwa3B3bW56eWd5bmp3c3poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDQwNjcsImV4cCI6MjA5NTQyMDA2N30.2__GFOakLAsFm1pMxRLcp_S4zM7uqyuTs0HfezFfmQA'
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.warn('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvcGVwa3B3bW56eWd5bmp3c3poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDQwNjcsImV4cCI6MjA5NTQyMDA2N30.2__GFOakLAsFm1pMxRLcp_S4zM7uqyuTs0HfezFfmQA')
-}
-
-export const supabase = createClient(supabaseUrl || 'https://yopepkpwmnzygynjwszh.supabase.co', supabaseAnonKey || 'placeholder')
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
